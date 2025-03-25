fee94a6c76866b87d82d885f7b5c827db32a1842
shantaisat <shanta.mahoneyis@gmail.com>
2025-03-25 04:12:35 +0100

    added hero covers to pages

--- silvol/settings.py
+++ silvol/settings.py
@@ -131,7 +131,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
